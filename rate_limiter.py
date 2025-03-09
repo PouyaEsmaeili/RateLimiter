adce9407a6bbc56ebb88496b2dac21301a2f3a97
Pouya <pouya.esmaeili.g@gmail.com>
2025-03-09 04:41:38 +0330

    Add uv tool. Add docstring.

--- rate_limiter.py
+++ rate_limiter.py
@@ -5,6 +5,45 @@ from string import ascii_uppercase, ascii_lowercase
 
 
 class RateLimiter(object):
+    """
+        A rate limiter implementation based on Redis using the leaky bucket algorithm.
+
+        This class limits the number of requests a client can make to a specific resource
+        within a defined time period. It supports client-specific rate limiting and global rate
+        limiting for resources.
+
+        Attributes:
+            con_pool (StrictRedis): The Redis connection pool used for Redis operations.
+            number_of_requests (int): The maximum number of allowed requests within the time_bound period.
+            time_bound (timedelta): The time window within which the requests are counted.
+            limit_per_client (bool): Flag indicating whether to limit requests per client or globally for the resource.
+            lock_timeout (float): Timeout in seconds for acquiring a lock to prevent race conditions.
+            log_value (str): The value to store in Redis when logging a request.
+
+        Methods:
+            log(client_id: str, resource_id: str) -> bool:
+                Logs a request for the client and resource if the rate limit is not exceeded. Returns True if allowed, False otherwise.
+
+            is_allowed(client_id: str, resource_id: str) -> bool:
+                Checks if a client is allowed to make a request to a resource based on the rate limit.
+
+            count_logs(client_id: str, resource_id: str) -> int:
+                Counts the number of logs associated with a client and resource in Redis.
+
+            flush_logs(client_id: str, resource_id: str) -> None:
+                Removes all logs related to a client and resource from Redis, resetting the rate limit.
+
+        Private Methods:
+            _get_lock_name(client_id: str, resource_id: str) -> str:
+                Generates a unique lock name based on the client and resource IDs.
+
+            _get_log_pattern(client_id: str, resource_id: str) -> str:
+                Generates the log pattern for counting logs in Redis.
+
+            _generate_log_name(client_id: str, resource_id: str) -> str:
+                Generates a unique log name with a random padding string to ensure uniqueness of logs.
+        """
+
     def __init__(self,
                  con_pool: StrictRedis,
                  number_of_requests: int,
